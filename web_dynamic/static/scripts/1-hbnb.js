c85a84fa08af242ad4031bf99fe3b073bcd2c890
Dhulkifli Abbas <dhulkifliabbas06@gmail.com>
2023-01-30 10:18:35 +0300

    advanced

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -2,12 +2,10 @@ amenities = new Map();
 
 $('input').change(checkboxChanged);
 
-function checkboxChanged(){
-    if (this.checked)
-        amenities.set($(this).attr("data-id"), $(this).attr("data-name"));
-    else{
-        amenities.delete($(this).attr("data-id"));
-    }
+function checkboxChanged () {
+  if (this.checked) { amenities.set($(this).attr('data-id'), $(this).attr('data-name')); } else {
+    amenities.delete($(this).attr('data-id'));
+  }
 
-    $(".amenities h4").text(Array.from(amenities.values()));
+  $('.amenities h4').text(Array.from(amenities.values()));
 }
--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -1,18 +1,53 @@
 $(document).ready(function () {
-  let selected = [];
   // checkboxes amenity
-  $('input[type=checkbox]').change(function () {
+  let selected = [];
+  let amId = [];
+  $('.amenities input[type=checkbox]').change(function () {
     $(this).each(function () {
       const name = $(this).attr('data-name');
       if ($(this).is(':checked')) {
         selected.push(name);
+        amId.push($(this).attr('data-id'));
       } else {
         selected = selected.filter(val => val !== name);
+        amId = amId.filter(val => val !== $(this).attr('data-id'));
       }
       if (selected.length === 0) {
         $('.amenities h4').text('\u00A0');
       } else {
         $('.amenities h4').text(selected.join(', '));
+      }
+    });
+  });
+  // checkboxes state
+  let stSelected = [];
+  let stateId = [];
+  $('.states input[type=checkbox]').change(function () {
+    $(this).each(function () {
+      const name = $(this).attr('data-name');
+      if ($(this).is(':checked')) {
+        stSelected.push(name);
+        stateId.push($(this).attr('data-id'));
+      } else {
+        stSelected = stSelected.filter(val => val !== name);
+        stateId = stateId.filter(val => val !== $(this).attr('data-id'));
+      }
+      if (stSelected.length === 0) {
+        $('.locations h4').text('\u00A0');
+      } else {
+        $('.locations h4').text(stSelected.join(', '));
+      }
+    });
+  });
+  // checkboxes city
+  // let ctSelected = [];
+  let cityId = [];
+  $('.cities input[type=checkbox]').change(function () {
+    $(this).each(function () {
+      if ($(this).is(':checked')) {
+        cityId.push($(this).attr('data-id'));
+      } else {
+        cityId = cityId.filter(val => val !== $(this).attr('data-id'));
       }
     });
   });
@@ -33,17 +68,32 @@ $(document).ready(function () {
     }
   });
   // build places
+  makePlaces({});
+  // filter places by selected amenities
+  $('button').click(function () {
+    makePlaces({ amenities: amId, states: stateId, cities: cityId });
+    // adding state and city filters
+  });
+});
+
+function makePlaces (dict) {
+  console.log('makePlaces arguments');
+  console.log(dict);
+  $('.places').empty();
+  $('.places').append('<h1>Places</h1>');
   $.ajax({
     url: 'http://0.0.0.0:5001/api/v1/places_search/',
     type: 'POST',
     contentType: 'application/json',
     dataType: 'json',
-    data: JSON.stringify({}),
+    data: JSON.stringify(dict),
     error: function (res) {
       $('.places').append('<p>Server issue</p>');
       console.log(res);
     },
     success: function (res) {
+      console.log('results');
+      console.log(res);
       let count = 0;
       $.each(res, function (k, v) {
         const article = $('<article>');
@@ -62,7 +112,7 @@ $(document).ready(function () {
       });
     }
   });
-});
+}
 
 function writeOwner (uid, count) {
   $.ajax({
